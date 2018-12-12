cae8c682de3d7b4af2d8f250b9bfe4f0e99b491a
Dewi Syarah Sofiati <dewissofiati@gmail.com>
2018-12-12 16:42:45 +0700

    add variable folder

---
+++ src/variable/DataTypeDeclaration.java
@@ -0,0 +1,12 @@
+package variable;
+
+public class DataTypeDeclaration{
+    
+    // inilah yang disebut deklarasi
+    public int serverPort;
+
+    public void setServerPort{
+        //inilai yang disebut inisialisasi
+        this.serverPort = 10;
+    }
+}
